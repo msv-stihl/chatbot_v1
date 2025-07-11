fcca38999f1fa98961288a4e9ec95710041385a5
msv-stihl <wesley.luz@manserv.com.br>
2025-07-11 15:24:17 -0300

    Delete atualizar_github.bat

--- atualizar_github.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\manserv\OneDrive - MANSERV\Área de Trabalho\Projetos_Wesley\chatbot\backend
-git pull
-git add dados.json
-git commit -m "Atualização automática de dados"
-git push origin main
